f22ed5bd1e0231993935586ba8b92be515d3c410
lorena <lugotlorenay@gmail.com>
2019-08-18 21:44:21 -0500

    Updates

---
+++ precache-manifest.644758286b6c6562bd3d6bb87c995273.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7b98a96ce292326b9164b394fd9aae",
+    "url": "/burger-queen-client/index.html"
+  },
+  {
+    "revision": "e9fee6204cc66ff551f3",
+    "url": "/burger-queen-client/static/css/main.7cfd5cf5.chunk.css"
+  },
+  {
+    "revision": "b376085c805375aff55c",
+    "url": "/burger-queen-client/static/js/2.4cdf5cc5.chunk.js"
+  },
+  {
+    "revision": "e9fee6204cc66ff551f3",
+    "url": "/burger-queen-client/static/js/main.e7cd4f4f.chunk.js"
+  },
+  {
+    "revision": "ce6c227d968e89f6471e",
+    "url": "/burger-queen-client/static/js/runtime~main.1077dcbe.js"
+  },
+  {
+    "revision": "3a2549a0f836364720ddbe5e15b291ca",
+    "url": "/burger-queen-client/static/media/hamburger.3a2549a0.png"
+  }
+]);
